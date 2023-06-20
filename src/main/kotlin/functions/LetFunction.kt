4687601af183bd632ce14507e8b867f059e864eb
Cristiano Castro <cscastro87@gmail.com>
2023-06-20 15:55:36 -0300

    Let function

--- src/main/kotlin/functions/LetFunction.kt
+++ src/main/kotlin/functions/LetFunction.kt
@@ -1,5 +1,14 @@
 package functions
 
 fun letFunction() {
+    val str = "ABCdef"
+
+    println(str)
+
+    str.let {
+        //scope function
+        //The var it references str
+        println(it.lowercase())
+    }
 
 }
