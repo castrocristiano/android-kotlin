2a1b724dd6cb107a9ef52b5563708557846c99b3
Cristiano Castro <cscastro87@gmail.com>
2023-06-22 17:24:08 -0300

    private modifier

--- src/main/kotlin/oo/Electronic.kt
+++ src/main/kotlin/oo/Electronic.kt
@@ -4,6 +4,8 @@ package oo
 open class Electronic(var manufacturer: String) {
      private var isOn: Boolean = false
 
+    fun getElectricStatus(): Boolean = isOn
+
     fun on() {
         if (!isOn)
             isOn = true
--- src/main/kotlin/oo/Main.kt
+++ src/main/kotlin/oo/Main.kt
@@ -2,7 +2,15 @@ package oo
 
 fun main() {
     val computer = Computer("Dell")
+
     println(computer.manufacturer)
+
+    println(computer.getElectricStatus())
+
+    if (!computer.getElectricStatus())
+        computer.on()
+
     computer.installSoftware()
+
     computer.off()
 }
